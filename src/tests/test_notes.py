fd90e291f9eb9d4861a56d6122232e768c32a98c
Abdullah M <abdullah_22@hotmail.com>
2023-04-25 12:16:19 +0100

    Finished Tutorial

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -89,6 +89,9 @@ def test_update_note(test_app, monkeypatch):
             [1, {}, 422],
             [1, {"description":"bar"}, 422],
             [999, {"title": "foo", "description":"bar"}, 404],
+            [1, {"title":"1", "description": "bar"}, 422],
+            [1, {"title":"foo", "description": "1"}, 422],
+            [0, {"title":"foo", "description": "bar"}, 422],
         ]
 )
 def test_update_note_invalid(test_app, monkeypatch, id, payload, status_code):
@@ -128,3 +131,6 @@ def test_remove_note_incorrect_id(test_app, monkeypatch):
     response = test_app.delete("/notes/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
